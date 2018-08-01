750e4da5a8676e7f2c8945ac2ba177eb4d2f8d66
Derrick F <>
2018-08-01 23:55:03 +0100

    use constance instead of variable.

--- exo_05/profile_test.go
+++ exo_05/profile_test.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 )
 
+const Message = "Education is what remains after one has forgotten what one has learned in school."
+
 var result string
-var message = "Education is what remains after one has forgotten what one has learned in school."
+
 func BenchmarkStringToUppper(b *testing.B) {
 	var r string
 	for i := 0; i < b.N; i++ {
-		r = strings.ToUpper(message)
+		r = strings.ToUpper(Message)
 	}
 	result = r
 }
